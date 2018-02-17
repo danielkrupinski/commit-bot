8b9463843fc348d6cb76459d5e86356ecc34a497
Daniel Krupiński <danielkrupinski@outlook.com>
2018-02-17 18:36:40 +0100

    Update code formatting

--- bot.sh
+++ bot.sh
@@ -4,10 +4,10 @@ touch file
 x=1
 while [ $x -le $[ amount-1 ] ]
 do
-	echo "bot" >> file
-	git add -A
-	git commit -m "commit"
-	x=$[x + 1]
+  echo "bot" >> file
+  git add -A
+  git commit -m "commit"
+  x=$[x + 1]
 done
 rm file
 git add -A
